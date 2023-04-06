8a68ddf05d93c65efbe18f2b859b3750e50eeb96
Vadim <vadim.penkin1997@mail.ru>
2023-04-06 23:21:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int Ticket = 10000; // Стоимость билета
+        int Milli = 20; // Стоимость 1 мили
+        int result = Ticket / Milli; // Сколько придет бонусов
+        System.out.println( result + "Ваши милли" );
+    }
+}
